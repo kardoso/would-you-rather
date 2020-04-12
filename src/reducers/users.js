50df25c94f6b4580aad1c1d83afe68b9ac30fbbf
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-12 15:09:11 -0300

    feat: Implement Reducers

---
+++ src/reducers/users.js
@@ -0,0 +1,36 @@
+import {
+  RECEIVE_USERS,
+  ADD_USER_QUESTION,
+  SAVE_USER_ANSWER,
+} from '../actions/users'
+
+export default function users(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_USERS:
+      return {
+        ...state,
+        ...action.users,
+      }
+    case ADD_USER_QUESTION:
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...state[action.authedUser],
+          questions: state[action.authedUser].questions.concat([action.qid]),
+        },
+      }
+    case SAVE_USER_ANSWER:
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...state[action.authedUser],
+          answers: {
+            ...state[action.authedUser].answers,
+            [action.id]: action.option,
+          },
+        },
+      }
+    default:
+      return state
+  }
+}
